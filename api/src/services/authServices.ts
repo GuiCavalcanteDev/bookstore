78ce95a3d87f1ac71d0156edd06845d741629c5b
Guilherme Cavalcante <guicavalcante59@gmail.com>
2024-11-20 16:46:55 -0300

    feat(user): criar services de usuario

---
+++ api/src/services/authServices.ts
@@ -0,0 +1,99 @@
+import { UserRepository } from '../repositories/userRepository';
+import { hashPassword, comparePassword } from '../helpers/hashHelper';
+import { createSession } from '../helpers/sessionHelper';
+import bcrypt from 'bcrypt';
+
+export class AuthService {
+  private userRepository: UserRepository;
+
+  constructor() {
+    this.userRepository = new UserRepository();
+  }
+
+  async registerUser(name: string, email: string, password: string) {
+    const passwordHash = await hashPassword(password);
+    const user = await this.userRepository.addUser(name, email, passwordHash);
+    return {
+      username: user.name,
+      email: user.email,
+      status: "Created"
+    };
+  }
+
+  async loginUser(email: string, password: string) {
+    const user = await this.userRepository.getUserByEmail(email);
+
+    if (!user) throw new Error('Usuário não encontrado');
+
+    const isPasswordValid = await comparePassword(password, user.passwordhash);
+    if (!isPasswordValid) throw new Error('Senha incorreta');
+
+    createSession(user.id);
+    return {
+      username: user.name,
+      email: user.email,
+      status: "Ok"
+    };
+  }
+
+  async getAllUsers() {
+    const users = await this.userRepository.getAllUsers();
+    return users.map(user => ({
+      id: user.id,
+      name: user.name,
+      email: user.email,
+    }));
+  }
+
+  async getUserById(id: number) {
+    const user = await this.userRepository.getUserById(id);
+    if (!user) throw new Error('Usuário não encontrado');
+
+    return {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+    };
+  }
+
+  async updateUser(id: number, updatedData: { name?: string; email?: string; password?: string }) {
+    const user = await this.userRepository.getUserById(id);
+    if (!user) {
+      throw new Error('Usuário não encontrado');
+    }
+
+    const updatedUser = {
+      ...user,
+      name: updatedData.name || user.name,
+      email: updatedData.email || user.email,
+      passwordhash: updatedData.password
+        ? hashPassword(updatedData.password)
+        : user.passwordhash,
+    };
+
+    await this.userRepository.updateUser(id, updatedUser.name, updatedUser.email, updatedUser.passwordhash);
+
+    return {
+      id: updatedUser.id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      status: 'Updated',
+    };
+  }
+
+  async deleteUser(id: number): Promise<{ status: string }> {
+    const user = await this.userRepository.getUserById(id);
+
+    if (!user) {
+      throw new Error('Usuário não encontrado');
+    }
+
+    const deleted = await this.userRepository.deleteUser(id);
+
+    if (!deleted) {
+      throw new Error('Erro ao deletar usuário');
+    }
+
+    return { status: 'Deleted' };
+  }
+}
